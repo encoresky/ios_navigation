eae0a804c2aaf1d4ce7dd1702320610397f75ddd
Ankush Lokhande <ankush.lokhande@encoresky.com>
2022-11-11 18:26:15 +0530

    Remove key

--- IosMapApp/IosMapApp/AppDelegate.swift
+++ IosMapApp/IosMapApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyB9DxyXJ1fgUZcl0QlomtyQ0WK2d8uBsco")
+        GMSServices.provideAPIKey("API_KEY")
         return true
     }
 
